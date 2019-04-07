7033a70af1ce10ee1e8ee4109833d2b0035169e7
Tam O'Donnell <thomashodonnell@gmail.com>
2019-04-07 20:49:15 +0100

    write code for test 16, pass test 16

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,3 +1,72 @@
+@customers = [
+  {
+    name: "Alice",
+    pets: [],
+    cash: 1000
+  },
+  {
+    name: "Bob",
+    pets: [],
+    cash: 50
+  }
+]
+
+@new_pet = {
+  name: "Bors the Younger",
+  pet_type: :cat,
+  breed: "Cornish Rex",
+  price: 100
+}
+
+@pet_shop = {
+  pets: [
+    {
+      name: "Sir Percy",
+      pet_type: :cat,
+      breed: "British Shorthair",
+      price: 500
+    },
+    {
+      name: "King Bagdemagus",
+      pet_type: :cat,
+      breed: "British Shorthair",
+      price: 500
+    },
+    {
+      name: "Sir Lancelot",
+      pet_type: :dog,
+      breed: "Pomsky",
+      price: 1000,
+    },
+    {
+      name: "Arthur",
+      pet_type: :dog,
+      breed: "Husky",
+      price: 900,
+    },
+    {
+      name: "Tristan",
+      pet_type: :dog,
+      breed: "Basset Hound",
+      price: 800,
+    },
+    {
+      name: "Merlin",
+      pet_type: :cat,
+      breed: "Egyptian Mau",
+      price: 1500,
+    }
+  ],
+  admin: {
+    total_cash: 1000,
+    pets_sold: 0,
+  },
+  name: "Camelot of Pets"
+}
+
+
+
+
 def pet_shop_name(shop)
   return shop[:name]
 end
@@ -33,12 +102,17 @@ def pets_by_breed(shop, breed)
 end
 
 def find_pet_by_name(shop, name)
+  pet_name = []
   for pet in shop[:pets]
     if pet[:name] == name
-      return pet
+      pet_name.push(pet)
     end
   end
+  return pet_name[0]
 end
+
+
+
 
 # def remove_pet_by_name(shop, name)
 #   for pet in shop[:pets]
